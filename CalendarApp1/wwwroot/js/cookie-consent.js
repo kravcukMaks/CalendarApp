cdee061b4bf1e8f763f715b1ca72063b9b26d107
Maksym2606 <vt221_kmr@student.ztu.edu.ua>
2025-04-01 18:40:52 +0300

    Added GDPR Cookie Popup using CookieConsent.js

---
+++ CalendarApp1/wwwroot/js/cookie-consent.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener("load", function () {
+    window.cookieconsent.initialise({
+        palette: {
+            popup: { background: "#222" },
+            button: { background: "#14a7d0" }
+        },
+        theme: "classic",
+        content: {
+            message: "Цей сайт використовує файли cookie для забезпечення найкращого досвіду користування.",
+            dismiss: "Зрозуміло",
+            link: "Дізнатися більше",
+            href: "/PRIVACY_POLICY.txt"
+        }
+    });
+});
